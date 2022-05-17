a2426b1d35b7ff563109dbd9a25f96f944da2857
VedantTrivedi14 <vedanttrivedi83065.com>
2022-05-17 14:34:26 +0530

    restructure code

---
+++ src/screens/UserScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text,View, StyleSheet} from "react-native";
+const UserScreen = () => {
+    return (
+      <View>
+    <Text >hi there!</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default UserScreen;  
